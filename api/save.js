f3b17dfffcb6fae8d15be9f0ae31ac285813728a
unknown <bkarshehbazgithub@gmail.com>
2022-08-08 15:01:01 +0500

    removed files

--- api/save.js
+++ api/save.js
@@ -1,59 +1,38 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
+const connectToDb = require("./lib/connect-to-db");
+const validate = require("./lib/validate-test");
+const dbCollection = process.env.MONGODB_COLLECTION;
 
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
+module.exports = async (req, res) => {
+  console.log("save hit");
 
-app.use(bodyParser.json());
+  if (req.method === "OPTIONS") {
+    res.send("ok!");
+    return;
+  }
 
-app.post("/", (req, res) => {
-  console.log(req.body.todo);
-});
-module.exports = app;
-// const connectToDb = require("./lib/connect-to-db");
-// const validate = require("./lib/validate-test");
-// const dbCollection = process.env.MONGODB_COLLECTION;
+  const { body: payload } = req;
 
-// module.exports = async (req, res) => {
-//   let data = [];
-//   req.on("data", chunk => {
-//     data.push(chunk);
-//   });
+  if (!payload) {
+    res.status(400).json({ type: "error", message: "Not a valid payload" });
+    return;
+  }
 
-//   console.log(data);
-//   res.end("pk");
-// if (req.method === "OPTIONS") {
-//   res.send("ok!");
-//   return;
-// }
+  const { error } = validate(payload);
+  const isValid = !error;
 
-// const { body: payload } = req;
+  if (!isValid) {
+    res.status(400).json({ type: "error", message: error });
+    return;
+  }
 
-// if (!payload) {
-//   res.status(400).json({ type: "error", message: "Not a valid payload" });
-//   return;
-// }
+  try {
+    const db = await connectToDb();
+    const collection = db.collection(dbCollection);
 
-// const { error } = validate(payload);
-// const isValid = !error;
-
-// if (!isValid) {
-//   res.status(400).json({ type: "error", message: error });
-//   return;
-// }
-
-// try {
-//   const db = await connectToDb();
-//   const collection = db.collection(dbCollection);
-
-//   const data = await collection.insertOne(payload);
-//   res.send({ id: data.insertedId });
-//   return;
-// } catch (error) {
-//   res.status(500).json({ type: "error", message: error.message });
-// }
-// };
+    const data = await collection.insertOne(payload);
+    res.send({ id: data.insertedId });
+    return;
+  } catch (error) {
+    res.status(500).json({ type: "error", message: error.message });
+  }
+};
